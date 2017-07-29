c6838edd64119337d7399dc337a921af73c747f9
shadowangle <shadowangle1222@hotmail.com>
2017-07-29 19:51:44 +0700

    asdasd

--- index.android.js
+++ index.android.js
@@ -29,10 +29,27 @@ export default class THEBALLYANDTHETRASH extends Component {
                 </View>
           </View>
         {/* End language bar */}
+
+        {/* start title bar */}
         <View style={styles.title}>
           <Text > CMU GREEN BIN </Text>
           </View>
+          {/* End title bar */}
 
+
+           <View style={styles.graphContainer}>
+          <Text > CMU GREEN BIN </Text>
+          </View>
+          <View style={styles.binContainer}>
+          <Text > CMU GREEN BIN </Text>
+          </View>
+          <View style={styles.statContainer}>
+          <Text > CMU GREEN BIN </Text>
+          </View>
+          <View style={styles.footerContainer}>
+
+          <Button title="Recycle item" /> 
+          </View>
 
 
       </View>
@@ -46,6 +63,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#338378',
   }, languageBar: {
 flexDirection:'row',
+flex:1,
   },language:{
     flex:1,
     alignItems: 'flex-end',
@@ -57,6 +75,43 @@ flexDirection:'row',
   },title:{
   flexDirection:'row',
   justifyContent: 'center',
+  flex:1,
+  }, graphContainer: {
+    flex:4,
+backgroundColor: 'white',
+justifyContent: 'center',
+flexDirection:'row',
+marginTop : 10,
+marginLeft : 30,
+marginRight : 30,
+  },
+binContainer: {
+    flex:1,
+backgroundColor: '#338378',
+justifyContent: 'center',
+flexDirection:'row',
+marginTop : 10,
+marginLeft : 30,
+marginRight : 30,
+  },
+statContainer: {
+    flex:3,
+backgroundColor: 'white',
+justifyContent: 'center',
+flexDirection:'row',
+marginTop : 10,
+marginLeft : 30,
+marginRight : 30,
+  },
+footerContainer: {
+    flex:1,
+backgroundColor: '#1fbba6',
+justifyContent: 'center',
+flexDirection:'row',
+marginTop : 10,
+marginLeft : 30,
+marginRight : 30,
+marginBottom : 20,
   }
   
 });
